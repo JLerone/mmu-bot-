08b6ae1e8a74e9555e8f5761740f7eaf2033e052
JudeWillow <lumenos1089@gmail.com>
2026-05-01 17:16:50 +0100

    Added kick and ban commands

--- messages.py
+++ messages.py
@@ -8,3 +8,15 @@ def setup(bot: commands.Bot):
     async def clear(ctx, amount: int):
         await ctx.channel.purge(limit=amount)
         await ctx.send(f"Deleted {amount} messages")
+
+    @bot.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(ctx, member: discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f"Kicked {member} | Reason: {reason}")
+
+    @bot.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(ctx, member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f"Banned {member} | Reason: {reason}")
